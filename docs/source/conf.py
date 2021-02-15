2432ab0beec9fde6b78168f138940cf5d52aa301
WingZHG <wingzhengho@hotmail.com>
2021-02-14 20:37:53 -0500

    ...

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,7 +52,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme_path = ['sphinx_material/sphinx_material']
+html_theme_path = ['sphinx_material']
 html_theme = 'sphinx_material'
 
 # Add any paths that contain custom static files (such as style sheets) here,
